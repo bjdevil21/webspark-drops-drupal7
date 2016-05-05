000e97713cec4f067f86ab22d53a2a1f5589058f
Michael Gilardi <mgilardi@asu.edu>
2016-05-05 23:22:08 +0000

    Changing node values and updating CSS in ASU Doc Request for new environment

--- sites/all/modules/asu_doc_request/asu_doc_request_pickup-shipping.js
+++ sites/all/modules/asu_doc_request/asu_doc_request_pickup-shipping.js
@@ -3,35 +3,35 @@
     attach: function (context, settings) {
 
     // If a form change occurs in paper transcripts shipping service radios.
-      // Hide shipping information if pickup options are selected.     
-      $('#edit-commerce-shipping-shipping-service-commerce-pickup-node-27189').change(function() {
+      // Hide shipping information if pickup options are selected.
+      $('#edit-commerce-shipping-shipping-service-commerce-pickup-node-4').change(function() {
         $('fieldset#edit-customer-profile-shipping').hide();
       });
 
-      $('#edit-commerce-shipping-shipping-service-commerce-pickup-node-27190').change(function() {
+      $('#edit-commerce-shipping-shipping-service-commerce-pickup-node-5').change(function() {
         $('fieldset#edit-customer-profile-shipping').hide();
       });
 
       $('#edit-commerce-shipping-shipping-service-us-mail').change(function() {
         $('fieldset#edit-customer-profile-shipping').show();
       });
-      
+
       $('#edit-commerce-shipping-shipping-service-us-postal-express-mail').change(function() {
         $('fieldset#edit-customer-profile-shipping').show();
       });
-      
+
       $('#edit-commerce-shipping-shipping-service-fedex-express').change(function() {
         $('fieldset#edit-customer-profile-shipping').show();
       });
-      
+
       $('#edit-commerce-shipping-shipping-service-international-fedex-express').change(function() {
         $('fieldset#edit-customer-profile-shipping').show();
       });
-      
+
       $('#edit-commerce-shipping-shipping-service-international-express-mail').change(function() {
         $('fieldset#edit-customer-profile-shipping').show();
       });
-      
+
     }
   }
 }(jQuery));
