63fcda427d15c37c070a8fecc4a59e3c015cb457
Bryan Roseberry <aubjr@asu.edu>
2018-03-09 09:26:24 -0700

    NEVADAUPDATE - Added ASU Doc request module changes to Git repo

--- sites/all/modules/asu_doc_request/asu_doc_request.js
+++ sites/all/modules/asu_doc_request/asu_doc_request.js
@@ -3,25 +3,6 @@
     attach: function (context, settings) {
 
 
-      // Handle show/hide on electronic recipients.
-
-      $('#field-transcript-recipient-coll-values tbody tr:not(:first)').hide();
-
-      $( "#edit-commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-e-coll-und-0-field-transcript-copies-p-und-0-value" ).change(function() {
-        $('#field-transcript-recipient-coll-values tbody tr').hide();
-
-        var count = $(this).val();
-	      var i = 0;
-
-	      while (i < count) {
-          $('#field-transcript-recipient-coll-values tbody tr:hidden:first').show();
-          i++;
-        }
-
-        $('#field-transcript-recipient-coll-values tbody tr:hidden input').val("");
-
-      });
-
 
       // Keep Shopping Cart Contents quantity and total in sync with form.
 
@@ -31,24 +12,37 @@
       var price = $('tbody td.views-field-commerce-unit-price');
       var unitPrice = price.html().trim().slice(1);
 
-      // If a form change occurs in delivery method radios or one of the # copies inputs.
-      $("input[name='commerce_fieldgroup_pane__group_transcript_processing_info[field_transcript_delivery_method][und]'], input#edit-commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-p-coll-und-0-field-transcript-copies-p-und-0-value, input#edit-commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-e-coll-und-0-field-transcript-copies-p-und-0-value").change(function(e){
+      // Handle show/hide on electronic recipients...
 
-          // Value selected on form. If a radio, select the appropriate quantity.
-          var selectedVal = e.currentTarget.value;
-          if (selectedVal == 'Electronic') {
-              var updatedQuantity = $('#edit-commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-e-coll-und-0-field-transcript-copies-p-und-0-value').val();
-          }
-          else if (selectedVal == 'Paper') {
-              var updatedQuantity = $('#edit-commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-p-coll-und-0-field-transcript-copies-p-und-0-value').val();
-          }
-          else { // Input value triggered change, just use it.
-              var updatedQuantity = selectedVal;
-          }
+      $('#field-transcript-recipient-coll-values tbody tr:not(:first)').hide();
+      
+        // ...after clicking 'up' or 'down' arrow
+      $("#commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-e-coll-und-0-field-transcript-copies-p-add-more-wrapper a.spinint-up, #commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-e-coll-und-0-field-transcript-copies-p-add-more-wrapper a.spinint-down").click(function() {
+        
+        $('#field-transcript-recipient-coll-values tbody tr').hide();
 
-          // Update the Shopping Cart Contents markup
-          quantity.html(updatedQuantity);
-          total.html('$' + (unitPrice * updatedQuantity));
+        var count = $('#commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-e-coll-und-0-field-transcript-copies-p-add-more-wrapper .spinint-scroller span.spinint').text();
+	      var i = 0;
+
+	      while (i < count) {
+          $('#field-transcript-recipient-coll-values tbody tr:hidden:first').show();
+          i++;
+        }
+
+        $('#field-transcript-recipient-coll-values tbody tr:hidden input').val("");
+
+        
+        quantity.html(count);
+        total.html('$' + (unitPrice * count));
+
+      });
+
+      $("#commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-p-coll-und-0-field-transcript-copies-p-add-more-wrapper a.spinint-up, #commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-p-coll-und-0-field-transcript-copies-p-add-more-wrapper a.spinint-down").click(function() {
+        
+        var count = $('#commerce-fieldgroup-pane-group-transcript-personal-info-field-transcript-info-p-coll-und-0-field-transcript-copies-p-add-more-wrapper .spinint-scroller span.spinint').text();
+
+        quantity.html(count);
+        total.html('$' + (unitPrice * count));
 
       });
 
