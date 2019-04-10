6eb480c5167c4f734bdf35a99f7ba1e85ad29f9c
Michael Gilardi <mgilardi@asu.edu>
2019-04-10 08:16:57 -0700

    Config changes for new setup on Enterprise

--- sites/all/modules/asu_comrf/js/asu_comrf.js
+++ sites/all/modules/asu_comrf/js/asu_comrf.js
@@ -1,9 +1,9 @@
 (function ($) {
   Drupal.behaviors.asu_comrf_ds_gray_bar = {
     attach : function(context, settings) {
-      // If page contains '.page-changingmajors-request' class AND does not contains the classes '.page-changingmajors-request-error' or '.page-changingmajors-request-confirmation'
-      if($(".page-changingmajors-request")[0]) {
-        if (!$(".page-changingmajors-request-notice")[0] && !$(".page-changingmajors-request-confirmation")[0]) {
+      // If page contains '.page-request' class AND does not contains the classes '.page-request-notice' or '.page-request-confirmation'
+      if($(".page-request")[0]) {
+        if (!$(".page-request-notice")[0] && !$(".page-request-confirmation")[0]) {
           // Inject offset div for Degree Search bar
           var full_width_ds_bar_offset = '<div class="asu-comrf-full-width-ds-bar-offset"></div>';
           $(document).one("ready", function() {
