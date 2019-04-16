23117c5964aff8e8251eea3f00248fa303d5b502
Michael Gilardi <mgilardi@asu.edu>
2019-04-16 13:42:31 -0700

    Added degree type data to student information and displaying it; fixing default campus and COM requirements when coming from DS; various form updates

--- sites/all/modules/.asu_comrf/js/asu_comrf.js
+++ sites/all/modules/.asu_comrf/js/asu_comrf.js
@@ -1,9 +1,8 @@
 (function ($) {
-  Drupal.behaviors.asu_comrf_request_form = {
+  Drupal.behaviors.asu_comrf_ds_gray_bar = {
     attach : function(context, settings) {
       // If page contains '.page-request' class AND does not contains the classes '.page-request-notice' or '.page-request-confirmation'
       if($(".page-request")[0]) {
-        // Only if we are not on the COM notice or confirmation pages
         if (!$(".page-request-notice")[0] && !$(".page-request-confirmation")[0]) {
           // Inject offset div for Degree Search bar
           var full_width_ds_bar_offset = '<div class="asu-comrf-full-width-ds-bar-offset"></div>';
@@ -11,13 +10,6 @@
             $('#block-asu-comrf-asu-comrf-student-info').before(full_width_ds_bar_offset);
           });
         }
-        // Force all external links on page to open in a new tab
-        $('a').each(function() {
-          var a = new RegExp('/' + window.location.host + '/');
-          if (!a.test(this.href)) {
-            $(this).attr("target","_blank");
-          }
-        });
       }
     }
   }
