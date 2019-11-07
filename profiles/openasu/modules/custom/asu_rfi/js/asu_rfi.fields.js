709a2071ea7ead45786c81cacbbc5f98cd7160ae
Bryan R <aubjr@asu.edu>
2019-11-06 17:04:38 -0700

    i1550 - dev j1

--- profiles/openasu/modules/custom/asu_rfi/js/asu_rfi.fields.js
+++ profiles/openasu/modules/custom/asu_rfi/js/asu_rfi.fields.js
@@ -11,12 +11,15 @@
             console.log(gaIds);
             var i, size, match;
             for (i = 0, size = gaIds.length, match = 0; i < size; i++) {
-              console.log(gaIds[i]);
               if (gaIds[i].get('trackingId') === 'UA-42798992-4') {
                 //The field name sent to the Lead API should be enterpriseclientid
                 //You can use an existing field or create it dynamically
-                alert(gaIds[1]);
+                console.log('this');
                 $('input[name="enterpriseclientid"]').val(gaIds[i].get('clientId'));
+                console.log(gaIds[i].valueOf('trackingId'));
+                console.log(gaIds[i].b.data.trackingId);
+                console.log(gaIds[i].valueOf('clientId'));
+                console.log(gaIds[i].b.data.clientId);
                 // This counter should be equal to the number of GA accounts to be read
                 if (++match === 1)
                   break;
