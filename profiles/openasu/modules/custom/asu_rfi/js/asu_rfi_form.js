5939f77d59d761c5b937bfb1c6bd531c0f0c47c1
Bryan R <aubjr@asu.edu>
2019-11-08 13:42:49 -0700

    WEBSPARK-1550 - ASU RFI: ENV check - fix jQuery dev 12

--- profiles/openasu/modules/custom/asu_rfi/js/asu_rfi_form.js
+++ profiles/openasu/modules/custom/asu_rfi/js/asu_rfi_form.js
@@ -6,10 +6,10 @@
       $('#take-me-to-rfi').on('click', function (e) {
         e.preventDefault();
         if ($('form[id^="asu-rfi-form-data"]').length === 0) {
-          console.log($(this).attr('id'));
+          setTimeout(console.log($(this).attr('id')), 5000);
           location.href = 'https://students.asu.edu/typeofstudent';
         } else {
-          console.log($(this).attr('id'));
+          setTimeout(console.log($(this).attr('id')), 5000);
           location.href = $(this).closest('form').attr('id');
         }
       });
