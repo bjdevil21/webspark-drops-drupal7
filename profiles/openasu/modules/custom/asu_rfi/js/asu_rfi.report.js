1653a2957bcdb55f6042569aad084d7ab4ac8408
Bryan R <aubjr@asu.edu>
2019-12-26 18:51:41 -0700

    DEV

--- profiles/openasu/modules/custom/asu_rfi/js/asu_rfi.report.js
+++ profiles/openasu/modules/custom/asu_rfi/js/asu_rfi.report.js
@@ -7,9 +7,12 @@
         });
       });
       $('.view-display-id-attachment_1 h4.questions', context).once('asuRfiReport', function() {
+        $(this).on("load", function() {
+          $(this).addClass('questions-collapsed').siblings('.questions-container').hide();
+        });
         $(this).on("click", function(e) {
           e.preventDefault();
-          $(this).siblings('.questions-container').toggle('fast', 'linear');
+          $(this).toggleClass('questions-collapsed').siblings('.questions-container').toggle('fast', 'linear');
         });
       });
       $('.view-asu-rfi-submissions-report .view-header p:empty, .view-header br').remove();
