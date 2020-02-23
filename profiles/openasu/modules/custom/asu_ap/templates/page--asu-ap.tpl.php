1858dcdde20eb2157c8674f0ee0dfd9672c9e9e4
Bryan R <aubjr@asu.edu>
2020-02-22 22:48:29 -0700

    AP tmp debug

--- profiles/openasu/modules/custom/asu_ap/templates/page--asu-ap.tpl.php
+++ profiles/openasu/modules/custom/asu_ap/templates/page--asu-ap.tpl.php
@@ -404,8 +404,6 @@ if (isset($node_info['field_asu_ap_market_text']['#items'][0]['safe_value'])) {
                         list($college_suffix, $college_prefix) = explode(", ", $college_raw);
                         $college = $college_prefix . " " . $college_suffix;
                         // Build in college URL if it exists
-                        dpm($node_info['field_asu_ap_college_j']['#items'], 'college_j');
-                        dpm($node_info['field_asu_ap_college_url_j']['#items'], 'url');
                         if (isset($node_info['field_asu_ap_college_url_j']['#items'][$college_index]['url'])) {
                           if (valid_url($node_info['field_asu_ap_college_url_j']['#items'][$college_index]['url'], TRUE)) {
                             $college = l(t($college), $node_info['field_asu_ap_college_url_j']['#items'][$college_index]['url'],
